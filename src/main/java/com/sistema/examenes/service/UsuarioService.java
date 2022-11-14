4c045f285d6d03fe9dcfd57176ae4f523b38a7f6
Andres <andreseguanipa1@gmail.com>
2022-11-13 21:11:08 -0400

    first commit

---
+++ src/main/java/com/sistema/examenes/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.sistema.examenes.service;
+
+import com.sistema.examenes.model.Usuario;
+import com.sistema.examenes.model.UsuarioRol;
+
+import java.util.Set;
+
+public interface UsuarioService {
+
+    public Usuario guardarUsuario(Usuario usuario, Set<UsuarioRol> usuarioRoles) throws Exception;
+
+    public Usuario obtenerUsuario(String username);
+
+    public void eliminarUsuario(Long usuarioId);
+}
