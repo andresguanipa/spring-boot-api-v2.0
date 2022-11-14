85c59ad8cdbe37bbab1374873ea68dbbf500c76c
Andres <andreseguanipa1@gmail.com>
2022-11-14 07:57:06 -0400

    testing git

--- src/main/java/com/sistema/examenes/controller/UsuarioController.java
+++ src/main/java/com/sistema/examenes/controller/UsuarioController.java
@@ -21,21 +21,26 @@ public class UsuarioController {
     @PostMapping("/")
     public Usuario guardarUsuario(@RequestBody Usuario usuario) throws Exception{
 
+
         usuario.setPerfil("default.png");
+
 
         Set<UsuarioRol> usuarioRoles = new HashSet<>();
 
+
         Rol rol = new Rol();
         rol.setRolId(2L);
-        
         rol.setRolNombre("NORMAL");
+
 
         UsuarioRol usuarioRol = new UsuarioRol();
         usuarioRol.setUsuario(usuario);
         usuarioRol.setRol(rol);
 
+
         usuarioRoles.add(usuarioRol);
         return usuarioService.guardarUsuario(usuario,usuarioRoles);
+        
     }
 
 
